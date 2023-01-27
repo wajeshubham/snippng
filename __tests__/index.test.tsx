a129183f8b15a74fbc3b206c561551097f7be392
Shubham Waje <waje.shubham111@gmail.com>
2023-01-27 12:18:01 +0530

    Modify tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,30 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import Home from "../pages/index";
+
+beforeAll(() => {
+  document.createRange = () => {
+    const range = new Range();
+
+    range.getBoundingClientRect = jest.fn();
+
+    range.getClientRects = () => {
+      return {
+        item: () => null,
+        length: 0,
+        [Symbol.iterator]: jest.fn(),
+      };
+    };
+
+    return range;
+  };
+});
 
 describe("LandingPage", () => {
   it("renders the landing page", async () => {
-    render(<Home />);
+    await act(async () => {
+      render(<Home />);
+    });
     await waitFor(() => {
       screen.getByText("Snippng");
     });

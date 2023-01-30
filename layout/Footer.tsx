30601e613494dd5bdb7ac1519f2f867752006087
Shubham Waje <waje.shubham111@gmail.com>
2023-01-30 13:28:26 +0530

    Fix footer style

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -3,30 +3,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-[0.1px] dark:border-zinc-200 border-zinc-300 bg-white dark:bg-black">
-      <div className="w-full h-16 gap-4 text-xs text-zinc-500 flex justify-center items-center max-w-7xl mx-auto px-4 sm:px-8 lg:px-12">
+    <footer className="w-full bg-transparent">
+      <div className="w-full h-16 gap-4 text-xs dark:text-zinc-400 text-zinc-600 flex justify-center items-center max-w-7xl mx-auto px-4 sm:px-8 lg:px-12">
         <Link
-          className="hover:underline"
+          className="hover:underline hover:dark:text-white hover:text-zinc-900"
           href="https://www.github.com/wajeshubham"
         >
           GitHub
         </Link>
         <Link
-          className="hover:underline"
+          className="hover:underline hover:dark:text-white hover:text-zinc-900"
           href="https://stackoverflow.com/users/13483939/shubham-waje"
         >
           Stack Overflow
         </Link>
         <Link
-          className="hover:underline"
+          className="hover:underline hover:dark:text-white hover:text-zinc-900"
           href="https://www.linkedin.com/in/shubham-waje/"
         >
           LinkedIn
         </Link>
-        <Link className="hover:underline" href="https://wajeshubham.in">
+        <Link
+          className="hover:underline hover:dark:text-white hover:text-zinc-900"
+          href="https://wajeshubham.in"
+        >
           Portfolio
         </Link>
-        <Link className="hover:underline" href="https://blog.wajeshubham.in">
+        <Link
+          className="hover:underline hover:dark:text-white hover:text-zinc-900"
+          href="https://blog.wajeshubham.in"
+        >
           Blogs
         </Link>
       </div>

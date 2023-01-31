58b1cdf0e07b75864729df962e3d628b5b44604f
Shubham Waje <waje.shubham111@gmail.com>
2023-01-31 15:26:48 +0530

    Add loading in snippet details page

--- pages/snippet/[uid].tsx
+++ pages/snippet/[uid].tsx
@@ -10,37 +10,47 @@ import { useRouter } from "next/router";
 
 const SavedSnippet = () => {
   const router = useRouter();
-  const [notFound, setNotFound] = useState(false);
-  const { uid } = router.query as { uid: string };
   const { user } = useAuth();
   const { setEditorConfig } = useSnippngEditor();
 
-  // TODO: handle loading
+  const [notFound, setNotFound] = useState(false);
+  const [loadingConfig, setLoadingConfig] = useState(true);
+
+  const { uid } = router.query as { uid: string };
+
   const fetchCodeSnippet = async () => {
-    if (!uid || !user) {
-      setNotFound(true);
+    if (!user) {
+      setLoadingConfig(false);
       return;
     }
-    if (!user) return;
     try {
       const docRef = await getDoc(doc(db, "user", user.uid, "snippets", uid));
       if (docRef.exists()) {
-        setEditorConfig(docRef.data() as SnippngEditorConfigInterface);
+        setEditorConfig({
+          ...docRef.data(),
+          uid,
+        } as SnippngEditorConfigInterface);
       }
     } catch (error) {
       setNotFound(true);
+    } finally {
+      setLoadingConfig(false);
     }
   };
 
   useEffect(() => {
-    if (!router.isReady) return;
+    if (!router.isReady || !user) return;
     fetchCodeSnippet();
-  }, [router.isReady]);
+  }, [router.isReady, user]);
 
   return (
     <Layout>
       {!notFound ? (
-        <SnippngCodeArea />
+        loadingConfig ? (
+          <p className="text-white">Loading...</p>
+        ) : (
+          <SnippngCodeArea />
+        )
       ) : (
         <p className="text-white">404 not found</p>
       )}

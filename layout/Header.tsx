e0a5d10018551049a85d7c516ba19423fbfb47ae
Shubham Waje <waje.shubham111@gmail.com>
2023-01-31 13:52:18 +0530

    Add profile page with get snippets and delet snippet api

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -2,22 +2,29 @@ import { Button, Logo, ThemeToggle } from "@/components";
 import GithubIcon from "@/components/icons/GithubIcon";
 import { useAuth } from "@/context/AuthContext";
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
-import { useContext } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const { loginWithGithub, user, logout } = useAuth();
+  const router = useRouter();
   return (
     <header data-testid="header" className="w-full">
       <nav className="w-full h-20 flex justify-between items-center max-w-7xl mx-auto px-4 sm:px-8 lg:px-12">
         <div className="flex justify-start items-center w-1/2 flex-shrink-0">
-          <Logo />
+          <Link href={"/"}>
+            <Logo />
+          </Link>
         </div>
         <div className="flex justify-end gap-4 items-center w-1/2 flex-shrink-0">
           <ThemeToggle />
           {user?.uid ? (
             <>
-              <Button>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
+              <Button
+                onClick={() => {
+                  router.push(`/profile`);
+                }}
+              >
                 <img
                   src={user.photoURL || ""}
                   alt="profile_image"

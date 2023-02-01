74ca7d23854440b5ec7be3c5f7a11c87a27903b0
Shubham Waje <waje.shubham111@gmail.com>
2023-02-01 12:46:37 +0530

    Add toast context with toast popup Remove unnecessary test cases

--- types/toast.ts
+++ types/toast.ts
@@ -1,1 +1,11 @@
 export type ToastVariantType = "success" | "error" | "info";
+
+export interface ToastInterface {
+  message: string;
+  description?: string;
+  type?: ToastVariantType;
+}
+
+export interface ToastContextInterface {
+  addToast: (toast: ToastInterface) => void;
+}

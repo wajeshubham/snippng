f25eade50da5cfedf8f751ca7b4fe96fa6719336
Shubham Waje <waje.shubham111@gmail.com>
2023-02-03 17:18:58 +0530

    Fix pwa HOC issue from next-pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
-const withPWA = require("next-pwa");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
-};
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV !== "production",
-    skipWaiting: true,
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV !== "production",
+  skipWaiting: true,
 });
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  // nextConfig
+  reactStrictMode: true,
+  output: "standalone",
+});

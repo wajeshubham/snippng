59af22c11e924ad20a503c3224f1c6dfb52922b3
wajeshubham <waje.shubham111@gmail.com>
2023-02-04 21:17:04 +0530

    Add desktop view by default for mobile devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          // content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          content="width=1024, shrink-to-fit=no, user-scalable=yes, viewport-fit=cover" // for default desktop view in mobile browsers
         />
       </Head>
       <ToastProvider>
